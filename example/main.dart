ffc965db3e7631136c5e59a3f001bb2d8dcd944e
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2023-07-11 20:52:53 -0400

    feat: add example folder

---
+++ example/main.dart
@@ -0,0 +1,44 @@
+import 'package:dart_mp_analytics/dart_mp_analytics.dart';
+
+void main() async {
+  // Initialize MPAnalytics options
+  const options = MPAnalyticsOptionsWeb(
+    clientId: 'your_client_id',
+    measurementId: 'your_measurement_id',
+    apiSecret: 'your_api_secret',
+  );
+
+  // Create an instance of MPAnalytics
+  final analytics = MPAnalytics(
+    options: options,
+    debugAnalytics: true, // Enable debug mode for testing
+    verbose: true, // Enable verbose logging
+  );
+
+  // Log an event
+  await analytics.logEvent(
+    'button_click',
+    parameters: {
+      'button_id': 'submit_button',
+      'page_name': 'home',
+    },
+  );
+
+  // Set user ID
+  analytics
+    ..setUserId('user123')
+    // Log another event with user properties
+    ..setUserProperty('membership', 'gold');
+  await analytics.logEvent(
+    'purchase',
+    parameters: {
+      'product_id': 'product123',
+      'price': 19.99,
+    },
+  );
+
+  // Clear user ID and user property
+  analytics
+    ..clearUserId()
+    ..removeUserProperty('membership');
+}
